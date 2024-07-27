e1def2562e6ecb1696aaaec1696bc65bfa5dda55
bacuna153 <bacuna153@gmail.com>
2024-07-27 15:42:36 +0100

    my third commit

--- fake1/src/App.js
+++ fake1/src/App.js
@@ -1,4 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
+import Form from './Form';
+// import List from './List';
+import Table from './Table';
 
 function App() {
   const API_URL = 'https://jsonplaceholder.typicode.com/';
@@ -8,13 +11,20 @@ function App() {
   useEffect(() => {
     const fetchItems = async() => {
       try{
-        const response = await fetch('${API_URL}')
+        const response = await fetch(`${API_URL}${reqType}`);
+        const data = await response.json();
+        setItems(data)
+      } catch(err){
+        console.log(err);
       }
     }
-  })
+    fetchItems();
+  }, {reqType})
   return (
     <div className="App">
-
+      <Form reqType={reqType} setReqType={setReqType} />
+      {/* <List items={items}/> */}
+      <Table items={items}/>
     </div>
   );
 }
